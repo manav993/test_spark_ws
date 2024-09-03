b0dc89e86db7c11b7b3fefe0cc452c434f5f2a5f
manav pandey <manav993@gmail.com>
2024-09-03 15:37:05 +0100

    Updated removed graph.html

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ consumer.on('message', function (message) {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-app.get('/graph.html', (req, res) => {
-  res.sendFile(__dirname + '/graph.html');
-});
 
 server.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
